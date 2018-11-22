20efefefdaa623d32b6ab533f6361c932a0d6f7b
Your Name <felipe.ferreira.ads@gmail.com>
2018-11-21 23:45:14 -0200

    parceiros

--- wp-content/themes/yoda/page-marca.php
+++ wp-content/themes/yoda/page-marca.php
@@ -22,5 +22,25 @@
             <img src="<?php echo get_template_directory_uri(); ?>/assets/img/bg-brand.jpg" class="bg" />
         </div>
     </section>
+    
+    <section class="partners">
+        <div class="container">
+            <h2>Parceiros</h2>
+            <div class="grid">
+                <a href="#" target="_blank">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/red-canids.png" alt="Red Canids">
+                </a>
+                <a href="#" target="_blank">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/twitch.png" alt="Twitch">
+                </a>
+                <a href="#" target="_blank">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logitech.png" alt="Logitech">
+                </a>
+                <a href="#" target="_blank">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/2-am.png" alt="2AM">
+                </a>
+            </div>
+        </div>
+    </section>
 
 <?php get_footer(); ?>
